4ca470bcfc6856c74ce0b34b4a83d5e859b377ca
Łukasz Sochacki <sochacki.luki@gmail.com>
2026-05-24 09:56:52 +0200

    Add routes in App.js

--- node-detailing-client/src/App.js
+++ node-detailing-client/src/App.js
@@ -6,18 +6,24 @@ import {
   getProjectsByCategory,
   getIsLoading,
 } from './redux/projectsRedux';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; // Importy z v7
 
 import IntroScreen from './components/views/IntroScreen/IntroScreen';
 import Header from './components/views/Header/Header';
 import ProjectFeed from './components/views/ProjectFeed/ProjectFeed';
 import ContactForm from './components/views/ContactForm/ContactForm';
 import AboutUs from './components/views/AboutUs/AboutUs';
+import AdminLogin from './components/pages/AdminLogin/AdminLogin'; // Widok logowania
+import AdminDashboard from './components/pages/AdminDashboard/AdminDashboard'; // Widok panelu
+import { getUser } from './redux/authRedux';
 
 const App = () => {
   const dispatch = useDispatch();
   const [showIntro, setShowIntro] = useState(true);
   const [currentCategory, setCurrentCategory] = useState('ALL');
 
+  // Pobieramy dane o zalogowanym użytkowniku (siostrze) z Reduxa
+  const user = useSelector(getUser);
   const filteredProjects = useSelector((state) =>
     getProjectsByCategory(state, currentCategory),
   );
@@ -33,34 +39,56 @@ const App = () => {
   }, [dispatch, showIntro]);
 
   return (
-    <>
-      {/* 2. OWIJAMY WARUNEK W ANIMATE PRESENCE */}
-      <AnimatePresence mode='wait'>
-        {showIntro ? (
-          // Przesyłamy klucz key, aby AnimatePresence wiedział, kiedy komponent znika
-          <IntroScreen key='intro' onComplete={() => setShowIntro(false)} />
-        ) : (
-          <div
-            key='content'
-            style={{ backgroundColor: '#FFFFFF', minHeight: '100vh' }}
-          >
-            <Header
-              currentCategory={currentCategory}
-              onCategoryChange={setCurrentCategory}
-            />
-
-            <main className='container-fluid px-0'>
-              {currentCategory === 'ABOUT US' ? (
-                <AboutUs />
+    <BrowserRouter>
+      <Routes>
+        {/* ŚCIEŻKA PUBLICZNA: Główna strona wizerunkowa z Twoją filmową animacją */}
+        <Route
+          path='/'
+          element={
+            <AnimatePresence mode='wait'>
+              {showIntro ? (
+                <IntroScreen
+                  key='intro'
+                  onComplete={() => setShowIntro(false)}
+                />
               ) : (
-                <ProjectFeed projects={filteredProjects} loading={isLoading} />
+                <div
+                  key='content'
+                  style={{ backgroundColor: '#FFFFFF', minHeight: '100vh' }}
+                >
+                  <Header
+                    currentCategory={currentCategory}
+                    onCategoryChange={setCurrentCategory}
+                  />
+
+                  <main className='container-fluid px-0'>
+                    {currentCategory === 'ABOUT US' ? (
+                      <AboutUs />
+                    ) : (
+                      <ProjectFeed
+                        projects={filteredProjects}
+                        loading={isLoading}
+                      />
+                    )}
+                  </main>
+                  {!isLoading && <ContactForm />}
+                </div>
               )}
-            </main>
-            {!isLoading && <ContactForm />}
-          </div>
-        )}
-      </AnimatePresence>
-    </>
+            </AnimatePresence>
+          }
+        />
+        {/* ŚCIEŻKA UKRYTA: Formularz logowania administratora. Jeśli siostra jest już zalogowana, przekieruje ją do panelu */}
+        <Route
+          path='/admin-login'
+          element={user ? <Navigate to='admin-dashboard' /> : <AdminLogin />}
+        />
+        {/* ŚCIEŻKA ZABEZPIECZONA: Panel zarządzania projektami. Jeśli siostra nie jest zalogowana, wyrzuci ją do ekranu logowania */}
+        <Route
+          path='/admin-dashboard'
+          element={user ? <AdminDashboard /> : <Navigate to='/admin-login' />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
